51d44e942df6dfbbecbef9eda70087dfa6e51af3
Nathaniel Fuller <31634490+fullern1@users.noreply.github.com>
2018-05-12 22:26:28 -0400

    Add files via upload

---
+++ CPP/CMSC341-Database Proj2/Driver.cpp
@@ -0,0 +1,18 @@
+// File: Driver.cpp
+//
+// CMSC 341 Spring 2017 Project 2
+//
+// Simple driver program to call the Sally Forth interpreter
+//
+
+
+#include <iostream>
+#include "Sally.h"
+
+int main() {
+  Sally S ;
+
+  S.mainLoop() ;
+
+  return 0 ;
+}
